96e34147b6c467b79632d3d5074a0e6286b96952
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-06-04 16:01:50 +0200

    Booklist

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Warning from './components/WarningSign';
+import MyBadge from './components/MyBadge'; //alberto
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <Warning text='This is warning button' />
+      <MyBadge />
+      <BookList />
     </div>
   );
 }
